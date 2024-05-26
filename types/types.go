51546e70a78e5acd83a0e92b5814199cba226a0e
vinicius-maker <viniviana75@hotmail.com>
2024-05-26 18:27:54 -0300

    structs para as api e retorno do canal

---
+++ types/types.go
@@ -0,0 +1,28 @@
+package types
+
+type ViaCepApi struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
+type BrasilCepApi struct {
+	Cep          string `json:"cep"`
+	State        string `json:"state"`
+	City         string `json:"city"`
+	Neighborhood string `json:"neighborhood"`
+	Street       string `json:"street"`
+	Service      string `json:"service"`
+}
+
+type Resultado struct {
+	Retorno interface{}
+	Origem  string
+}
